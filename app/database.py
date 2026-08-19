9e0f4914667f9de7331e895362255cb8f21c7bf8
Kaushal <kbraju11@hotmail.com>
2026-08-19 09:41:46 -0400

    database

--- app/database.py
+++ app/database.py
@@ -6,6 +6,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DATABASE_URL =  os.getenv("DATABASE_URL")
+print("LENGTH:", len(url) if url else "MISSING")
+if url and "@" in url:
+    host_part = url.split("@")[1].split("/")[0]
+    print("HOST PART LENGTH:", len(host_part))
+    print("HOST PART FIRST CHAR:", host_part[0] if host_part else "EMPTY")
+else:
+    print("NO @ FOUND IN URL")
 
 engine = create_engine(DATABASE_URL)
 
