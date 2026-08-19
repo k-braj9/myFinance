e494e67f0347230a7615c213e59320db1dd5497b
Kaushal <kbraju11@hotmail.com>
2026-08-19 10:45:19 -0400

    new_change

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "https://my-finance-p6yl-seven.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
